e717d56267c0a416135e93b8fb86625a3ba1e9bc
Jittapan Pluemsumran <nightsheep@outlook.com>
2018-05-04 00:52:20 +0700

    Fixed "No Data" being checked and "Very High" being impossible to be checked. Fixes #1.

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ rating = prompt("How much do you like this teacher? Enter any number between 1 t
 if (rating < 1 || rating > 5) throw new Error("Enter only 1-5! Please run the script again!");
 for (i = 0; i <= 23; i++) {
     rand = ((Math.floor(Math.random() * 10)) % 3) + (5 - rating - 1);
-    if (rand > 5) rand = 5;
-    if (rand < 1) rand = 1;
+    if (rand > 4) rand = 4;
+    if (rand < 0) rand = 0;
     document.getElementById("RBL" + i + "_" + rand).checked = true;
 };
