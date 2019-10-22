ad130ea690d023acd6d1094cc020e0501f698441
Jittapan Pluemsumran <secret@jittapan.app>
2019-10-22 12:12:17 +0700

    Ask if the user want to randomize the score ;)

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 rating = prompt("How much do you like this teacher? Enter any number between 1 to 5 ( 5 = highest, 1 = lowest )", 3);
 if (rating < 1 || rating > 5) throw new Error("Enter only 1-5! Please run the script again!");
+randomRating = confirm("Do you want to randomize the scroll for a bit?");
 for (i = 0; i <= 23; i++) {
-    rand = ((Math.floor(Math.random() * 10)) % 3) + (5 - rating - 1);
+    rand = randomRating ? ((Math.floor(Math.random() * 10)) % 3) + (5 - rating - 1) : rating;
     if (rand > 4) rand = 4;
     if (rand < 0) rand = 0;
     document.getElementById("RBL" + i + "_" + rand).checked = true;
